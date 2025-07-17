8b93f01d99ac774096b6732ac9218547ed31e730
Prathamesh Kadam <146866950+PrathameshKadam-07@users.noreply.github.com>
2025-07-17 20:39:51 +0530

    Add files via upload

---
+++ CALCULATOR/calculator.js
@@ -0,0 +1,69 @@
+function one(){
+    var a =1;
+    document.getElementById("area").value += a;
+}
+function two(){
+    var a =2;
+    document.getElementById("area").value += a;
+}
+function three(){
+    var a =3;
+    document.getElementById("area").value += a;
+}
+ function C(){
+   var a = document.getElementById("area").value;
+    document.getElementById("area").value = a.slice(0, -1);
+ }
+function ans(){
+    document.getElementById("area").value = eval(document.getElementById("area").value);  
+}
+
+function AC(){
+    document.getElementById("area").value = null;
+}
+function four(){
+    var a =4;
+    document.getElementById("area").value += a;
+}
+function five(){
+    var a =5;
+    document.getElementById("area").value += a;
+}
+function six(){
+    var a =6;
+    document.getElementById("area").value += a;
+}
+function seven(){
+    var a =7;
+    document.getElementById("area").value += a;
+}
+
+function eight(){
+    var a =8;
+    document.getElementById("area").value += a;
+}
+function nine(){
+    var a =9;
+    document.getElementById("area").value += a;
+}
+function zero(){
+    var a =0;
+    document.getElementById("area").value += a;
+}
+function add(){
+    var a = "+";
+    document.getElementById("area").value += a;
+}
+function sub(){
+    var a ="-";
+    document.getElementById("area").value += a;
+}
+function mul(){
+    var a ="*";
+    document.getElementById("area").value += a;
+}
+function div(){
+    var a ="/";
+    document.getElementById("area").value += a;
+}
+
